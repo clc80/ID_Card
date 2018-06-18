d2580b0b0499f618d99b1204a45aa50c95d57fe7
clc80 <clc_80@yahoo.com>
2018-06-17 19:20:07 -0700

    Fetched Data from API

--- src/App.js
+++ src/App.js
@@ -3,13 +3,48 @@ import IdCard from './components/idCard';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <IdCard />
-      </div>
-    );
-  }
+  constructor(props) {
+   super(props);
+   this.state = {
+     error: null,
+     isLoaded: false,
+     person: []
+   };
+ }
+
+ componentDidMount() {
+   fetch("https://randomuser.me/api/")
+     .then(res => res.json())
+     .then(
+       (data) => {
+         this.setState({
+           isLoaded: true,
+           person: data.results
+         });
+       },
+       (error) => {
+         this.setState({
+           isLoaded: true,
+           error
+         });
+       }
+     )
+ }
+
+ render() {
+   const { error, isLoaded, person } = this.state;
+   if (error) {
+     return <div>Error: {error.message}</div>;
+   } else if (!isLoaded) {
+     return <div>Loading...</div>;
+   } else {
+     return (
+       <main>
+         {console.log({person})}
+       </main>
+     );
+   }
+ }
 }
 
 export default App;
